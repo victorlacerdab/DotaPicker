32d0d38849234aa7dc2091f5003532c01b29f1d0
Victor Lacerda <victorlbotelho@gmail.com>
2024-08-03 00:02:53 +0200

    Cleaned the data.

---
+++ src/utils.py
@@ -0,0 +1,58 @@
+import json
+import pandas as pd
+from pandas import DataFrame
+
+def match_picks_cleaner(dframe: DataFrame) -> DataFrame:
+    dframe = dframe[['is_pick', 'hero_id', 'team', 'order', 'match_id']]
+    return dframe
+
+def match_results_cleaner(dframe: DataFrame) -> DataFrame:
+    dframe = dframe[['match_id', 'radiant_win']]
+    return dframe
+
+def load_json(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return dict(data)
+
+class DotaTokenizer:
+    def __init__(self, metadata: dict, special_tks: list):
+        self.simple_vocab = self.get_vocab(metadata, special_tks, alt_vocab=False)
+        self.pickban_vocab = self.get_vocab(metadata, special_tks, alt_vocab=True)
+    
+    def encode(self, match, alt: bool) -> str:
+        ''' 
+        Encodes the pick/bans in the match according to the desired tokenization method.
+        '''
+        if not alt:
+            pass
+        else:
+            pass
+        
+        return None
+
+    def decode(self, alt: bool) -> str:
+        pass
+
+    def get_vocab(self, metadata: dict, special_tks: list, alt_vocab: bool) -> dict:
+        
+        vocab = {}
+        idx_to_ignore = len('npc_dota_hero_')
+        counter = len(special_tks)
+
+        for i, tk in enumerate(special_tks):
+            vocab.update({i: tk})
+
+        for k,v in metadata.items():
+            v = v['name'][idx_to_ignore:]
+            if not alt_vocab:
+                vocab.update({int(k) + len(special_tks): v})
+            else:
+                vocab.update({counter: f'[PICK_{v}]'})
+                counter += 1
+                vocab.update({counter: f'[BAN_{v}]'})
+                counter += 1
+
+        return vocab
+
+            
